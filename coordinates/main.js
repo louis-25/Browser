8b0c7f0ff4cc4310bb19218286b3408afc6d6400
louis-25 <20151577@vision.hoseo.edu>
2021-02-05 17:42:27 +0900

    coordinates 성능개선

--- coordinates/main.js
+++ coordinates/main.js
@@ -3,16 +3,28 @@ const horozontal = document.querySelector('.horozontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
-document.addEventListener('mousemove', (event)=> {
-    const x = event.clientX;
-    const y = event.clientY;
-    // console.log(`${x} ${y}`);
+const targetRect = target.getBoundingClientRect();
+console.log(targetRect);
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
 
-    vertical.style.left = `${x}px`;
-    horozontal.style.top = `${y}px`;
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
-    tag.style.top = `${y}px`;
-    tag.style.left = `${x}px`;
-    tag.innerHTML = `${x}px ${y}px`;
+addEventListener('load',() => {
+    document.addEventListener('mousemove', (event)=> {
+        const x = event.clientX;
+        const y = event.clientY;
+        // console.log(`${x} ${y}`);
+    
+        vertical.style.transform = `translateX(${x}px)`;
+        horozontal.style.transform = `translateY(${y}px)`;
+        target.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;          
+        tag.style.transform = `translate(${x}px, ${y}px)`;          
+        tag.innerHTML = `${x+10}px ${y+10}px`;
+        // vertical.style.left = `${x}px`;
+        // horozontal.style.top = `${y}px`;
+        // target.style.left = `${x}px`;
+        // target.style.top = `${y}px`;
+        // tag.style.top = `${y}px`;
+        // tag.style.left = `${x}px`;
+        // tag.innerHTML = `${x}px ${y}px`;
+    });
 });
