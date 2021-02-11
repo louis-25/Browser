7586bab9a26ac068ca38bcc45d01e2a1838e8c2c
louis-25 <20151577@vision.hoseo.edu>
2021-02-11 23:14:55 +0900

    module: game

--- carrot/src/main.js
+++ carrot/src/main.js
@@ -1,133 +1,34 @@
 'use strict';
 
 import PopUp from './popup.js';
-import Field from './field.js';
-import * as sound from './sound.js';
+import Game from './game.js';
 
 const CARROT_COUNT = 5;
 const BUG_COUNT = 5;
 const GAME_DURATION_SEC = 5;
 
-const gameBtn = document.querySelector('.game__button');
-const gameTimer = document.querySelector('.game__timer');
-const gameScore = document.querySelector('.game__score');
-
-let started = false;
-let score = 0;
-let timer = undefined;
-
 const gameFinishBanner = new PopUp();
+
+const game = new Game(GAME_DURATION_SEC, CARROT_COUNT, BUG_COUNT);
+game.setGameStopListener((reason) => {
+    console.log(reason);
+    let message;
+    switch(reason) {
+        case 'cancel':
+            message = 'Replay?';
+            break;
+        case 'win':
+            message = 'YOU WON!';
+            break;
+        case 'lose':
+            message = 'YOU LOST';
+            break;
+        default:
+            throw new Error('not valid reason');           
+    }
+    gameFinishBanner.showWithText(message);
+});
+
 gameFinishBanner.setClickListener(()=> {
-    startGame();
+    game.start();
 });
-
-const gameField = new Field(CARROT_COUNT, BUG_COUNT);
-gameField.setClickListener(onItemClick);
-
-function onItemClick(item) { //이벤트위임
-    if(!started) {
-        return;
-    }    
-    if(item === 'carrot') { //당근        
-        score++;        
-        updateScoreBoard();
-        if(score === CARROT_COUNT) {
-            finishGame(true);
-        }
-    } else if(item === 'bug') { //벌레                
-        finishGame(false);
-    }
-}
-
-//gameField.addEventListener('click', onFieldClick);
-gameBtn.addEventListener('click', () => {    
-    initGame();
-    if(started) {
-        stopGame();
-    } else {
-        startGame();
-    }    
-});
-
-function startGame() {
-    started = true;
-    initGame();
-    startGameTimer();
-    showStopButton();
-    showTimerAndScore();
-    gameField.init();
-    sound.playBackground();
-}
-
-function stopGame() {
-    started = false;    
-    stopGameTimer();
-    hideGameButton();
-    gameFinishBanner.showWithText('REPLAY?');    
-    sound.playAlert();
-    sound.stopBackground();
-}
-
-function finishGame(win) {
-    started = false;        
-    if(win) {        
-        sound.playWin();
-        hideGameButton(); 
-    }else {        
-        sound.playBug();   
-        hideGameButton(); 
-    }
-    stopGameTimer();
-    sound.stopBackground();
-    gameFinishBanner.showWithText(win? 'YOU WON' : 'YOU LOST');    
-}
-
-function showStopButton() {
-    const icon = gameBtn.querySelector('.fas');
-    icon.classList.add('fa-stop');
-    icon.classList.remove('fa-play');
-    gameBtn.style.visibility = 'visible';
-}
-
-function hideGameButton() {
-    gameBtn.style.visibility = 'hidden';
-}
-
-function showTimerAndScore() {
-    gameTimer.style.visibility = 'visible';
-    gameScore.style.visibility = 'visible';
-}
-
-function startGameTimer() {    
-    let remainingTimeSec = GAME_DURATION_SEC;
-    updateTimerText(remainingTimeSec);
-    timer = setInterval(()=>{
-        if(remainingTimeSec <= 0) {
-            clearInterval(timer);
-            finishGame(CARROT_COUNT === score);
-            return;
-        }
-        updateTimerText(--remainingTimeSec);
-    }, 1000);    
-}
-
-function stopGameTimer() {
-    clearInterval(timer);    
-}
-
-function updateTimerText(time) {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-    gameTimer.innerText = `${minutes}:${seconds}`
-}
-
-function initGame() {
-    score = 0;    
-    gameScore.innerText = CARROT_COUNT;           
-
-    //벌레와 당근을 생성한뒤 field에 추가해줌    
-}
-
-function updateScoreBoard() {
-    gameScore.innerText = CARROT_COUNT - score;
-}
